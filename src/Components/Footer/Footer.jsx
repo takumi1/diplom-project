c0efb90afddbfd1a875e10a6508bc3479ba63dc7
takumi1 <avastr@yandex.ru>
2022-10-27 17:32:24 +0700

    Готовый дипломный проект.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,11 +14,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className={s.payment}>
-                <div className={s.paymentHeader}>Принимаем к оплате:</div>
-                <div className={s.paymentSprite}>
-                    <div className={s.paymentFirst}></div>
-                    <div className={s.paymentSecond}></div>
-                </div>
+
                 <div className={s.shopDescription}>
                     2009-2019 © BosaNoga.ru — модный интернет-магазин обуви и аксессуаров. Все права защищены. <br/>
                     Доставка по всей России!
